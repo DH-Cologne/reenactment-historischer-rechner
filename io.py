f5df7305826635022e1b7ec695df7333cd4bfaa0
Sara Saleh <ssaleh2@smail.uni-koeln.de>
2022-05-10 12:09:49 +0200

    added files

---
+++ io.py
@@ -0,0 +1,38 @@
+def printMemory(auswahl):
+    '''print state of memory and registers'''
+    # memory = memory.getAll()
+    # register = cpu.getRegister()
+    # if auswahl == 0:
+    #     pass
+    # elif auswahl == 1:
+    #     pass
+    # elif auswahl == 2:
+    #     pass
+    pass
+    
+def readProgram(txt):
+    'reads in the program, saves it into the memory and executes Bandbefehle'
+    # Abwandlung von bandbefehle.py
+    pass
+    #return startingPoint
+
+
+class ResultPrinter:
+    def __init__(self):
+        self.strToPrint =""
+
+    def collectPrint(self):
+        '''adds the accumulator to strToPrint'''
+        #self.strToPrint += memory.get()
+
+        pass
+    def printAll(self):
+        '''prints all into a text file'''
+        #print(self.strToPrint)
+        # with open("result.txt", "w") as f:
+        #     f.write(self.strToPrint)  
+        pass    
+
+if __name__ == '__main__':
+
+    b = ResultPrinter()
---
+++ z22.py
@@ -0,0 +1,10 @@
+# import io
+# import cpu
+# import memory
+
+# memory = memory.Memory()
+# printer = io.Printer()
+# cpu = cpu.CPU()
+
+# cpu.start(io.readProgram, memory, printer)
+# io.printAll()
