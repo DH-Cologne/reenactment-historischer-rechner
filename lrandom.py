6ba56d7af67f1136fcfb84a01ae8e3de2fe9a0b9
Nils Reiter <nils.reiter@uni-koeln.de>
2022-05-23 07:55:56 +0200

    Lutz Random

--- lrandom.py
+++ lrandom.py
@@ -4,12 +4,14 @@ class LutzRandom:
   
   def nextnumber(self):
     x1 = ((self.x << 4) + self.x) >> 4
-    self.x = x1
-    # print("{0:b}".format(x1))
+    self.x = x1 & (2**38-1)
     return (x1 & 15)
   
   
 if __name__ == "__main__":
   lr = LutzRandom()
   for i in range(10000):
-    print(lr.nextnumber())
+    r = lr.nextnumber()
+    s = "{0:b}".format(lr.x)
+    print(str(i) + ": x = " + s+ " ("+str(len(s))+") r = " + str(r))
+    #print(r)
