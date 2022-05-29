20fa5e194136650e3d4878d22e0f78f11e670655
ChristineSchaefer <kristus4711@googlemail.com>
2022-05-29 17:36:04 +0200

    Binärumwandlung Befehl

--- wort.py
+++ wort.py
@@ -82,19 +82,158 @@ class Wort:
 
 class Befehl(Wort):
 
-    # benötigen wir den Schnellspeicher?
-
     def __init__(self, zelle, strWort):
         super().__init__(zelle, strWort)
-
-    def isAddition(self):
-        pass
+        self.isArithmetic = False
+        self.isJumpOrCall = False
+        self.isCondition = False
+        self.isStop = False
+        self.isShift = False
 
     def getAddons(self):
+        # Was ist hiermit gemeint?
         pass
 
     def getBinary(self):
-        pass
+        """
+        Umwandlung eines Befehls als String in eine Binärzahl.
+        :return: Befehl als Binärzahl
+        """
+        # Befehl wird aus String erstellt (Operation + Adresse)
+        befehl = self.strWort
+
+        # Befehle beginnen mit [1,0]
+        binary = [1, 0]
+
+        # Initialisierung von Binärzahl-Sequenzen, die dann mit Inhalt gefüllt werden
+        operation = list
+        speicher = list
+
+        # trennen von Operations- + Adressteil
+        match = re.match(r"([a-z]+)(\d+)?([+]\d+)?([a-z]+)?", befehl, re.I)
+        if match:
+            items = match.groups()
+            items = [i for i in items if i is not None]
+
+            # Umwandlung in Binärzahl
+            operation = self.encode_operation([i for i in items if i.isalpha()])
+            speicher = self.encode_address([i for i in items if i.isnumeric() | i.__contains__('+')])
+
+        # Zusammenfügen der einzelnen Teile zu einer Binärzahl
+        binary += operation + speicher
+        return binary
+
+    def encode_operation(self, operation_list):
+        """
+        String Befehl wird zu 18-stelliger Binärzahl umgewandelt.
+        :param operation_list Liste mit allen genannten Operationen einer Speicherzelle
+        :return: operation als Binärzahl
+        """
+        # Rückgabe-Liste: wird mit 0 und 1 an entsprechenden Stellen gefüllt
+        operation = [0] * 18
+        for o in operation_list:
+            # Überprüfung der einzelnen Befehle
+            if o.__contains__('PP'):
+                operation[1] = 1
+                self.isCondition = True
+            if o.__contains__('P'):
+                operation[2] = 1
+                self.isCondition = True
+            if o.__contains__('QQ'):
+                operation[3] = 1
+                self.isCondition = True
+            if o.__contains__('Q'):
+                operation[4] = 1
+                self.isCondition = True
+            if o.__contains__('Y'):
+                operation[5] = 1
+                self.isCondition = True
+            if o.__contains__('C'):
+                operation[6] = 1
+                self.isCondition = True
+            if o.__contains__('N'):
+                operation[7] = 1
+                self.isShift = True
+            if o.__contains__('LL'):
+                operation[8] = 1
+                self.isShift = True
+            if o.__contains__('R'):
+                operation[9] = 1
+                self.isShift = True
+            if o.__contains__('U'):
+                operation[10] = 1
+                self.isArithmetic = True
+            if o.__contains__('A'):
+                operation[11] = 1
+                self.isArithmetic = True
+            if o.__contains__('S'):
+                operation[12] = 1
+                self.isArithmetic = True
+            if o.__contains__('F'):
+                operation[13] = 1
+                self.isJumpOrCall = True
+            if o.__contains__('K'):
+                operation[14] = 1
+                self.isCondition = True
+            if o.__contains__('H'):
+                operation[15] = 1
+                self.isCondition = True
+            if o.__contains__('Z'):
+                operation[16] = 1
+                self.isStop = True
+            if o.__contains__('G'):
+                operation[17] = 1
+                self.isCondition = True
+            if o.__contains__('V'):
+                operation[18] = 1
+                self.isCondition = True
+
+        return operation
+
+    def encode_address(self, address_list):
+        """
+           String Speicheradresse wird zu 18-stelliger Binärzahl umgewandelt.
+           Unterteilung des Schnell- (5-stellig) und Trommelspeichers (13-stellig).
+           :param address_list Liste mit allen genannten Speicherzellen
+           :return: adresse als Binärzahl
+           """
+        # Initialisierung der Speicher, die mit Inhalt gefüllt werden
+        schnellspeicher = list()
+        trommelspeicher = list()
+
+        for a in address_list:
+            # wenn mehrere Speicherzellen angegeben sind, wird die zweite mit '+' angegeben
+            # damit die Speicherzelle als Integer weiterverarbeitet werden kann, wird das '+' entfernt
+            if a.__contains__('+'):
+                a.replace('+', '')
+            # Umwandlung des Strings in einen Integer
+            a = int(a)
+            # Trommelspeicher
+            if a > 32:
+                # Umwandlung in Binärzahl
+                binary = format(a, 'b')
+                # Trommelspeicher hat insgesamt 13 Stellen. Die, die nicht für die Binärzahl gebraucht werden,
+                # werden mit 0 gefüllt
+                trommelspeicher = [0] * (13 - len(binary))
+                trommelspeicher += list(map(int, list(binary)))
+            # Schnellspeicher
+            elif a <= 32:
+                # Umwandlung in Binärzahl
+                binary = format(a, 'b')
+                # Schnellspeicher hat insgesamt 5 Stellen. Die, die nicht für die Binärzahl gebraucht werden,
+                # werden mit 0 gefüllt
+                schnellspeicher = [0] * (5 - len(binary))
+                schnellspeicher += list(map(int, list(binary)))
+
+        # falls einer der Speicher leer ist, wird dieser mit 0 gefüllt
+        if not trommelspeicher:
+            trommelspeicher = [0] * 13
+        if not schnellspeicher:
+            schnellspeicher = [0] * 5
+
+        # Zusammenfügen zu einer Binärzahl
+        adresse = schnellspeicher + trommelspeicher
+        return adresse
 
 
 class Klartext(Wort):
@@ -147,7 +286,7 @@ class Ganzzahl(Wort):
 
         # does not include handling of float humbers
         float_rep = float(self.strWort)
-        if abs(float_rep) > (2**35) - 1:
+        if abs(float_rep) > (2 ** 35) - 1:
             raise Exception(f"Strichzahl {float_rep} is out of bound.")
 
         bin_number = list(bin(int(self.strWort)).split("b")[1].strip(" "))
@@ -182,11 +321,20 @@ if __name__ == '__main__':
     w3 = Wort(3, 'D')
     w4 = Wort(4, 'SCHLOS ')
     w5 = Wort(5, '2\'')
+    w6 = Wort(6, 'B0+1900')
+    w7 = Wort(7, 'E1720E')
+
     print('Typ von String {} ist {}'.format(w1.strWort, type(w1.parse())))
     print('Typ von String {} ist {}'.format(w2.strWort, type(w2.parse())))
     print('Typ von String {} ist {}'.format(w3.strWort, type(w3.parse())))
     print('Typ von String {} ist {}'.format(w4.strWort, type(w4.parse())))
     print('Typ von String {} ist {}'.format(w5.strWort, type(w5.parse())))
+    print('Typ von String {} ist {}'.format(w6.strWort, type(w6.parse())))
 
     print(w5.parse().getBinary())
     print(w4.parse().getBinary())
+    # print(w1.parse().getBinary())
+    # print(w6.parse().getBinary())
+    print(w2.parse().getBinary())
+    print(w3.parse().getBinary())
+    print(w7.parse().getBinary())
