02be14b00b9d04c00803c754dce8e87d3159b018
Sara Saleh <ssaleh2@smail.uni-koeln.de>
2022-06-16 17:35:47 +0200

    added print options

--- input_output.py
+++ input_output.py
@@ -2,8 +2,10 @@ import io
 import os.path
 import re
 import csv
+from prettytable import PrettyTable
 
 #import memory
+#import wort
 
 
 class IoMemory:
@@ -17,32 +19,67 @@ class IoMemory:
         memory_dict = memory.getAll()
         for speicherzelle, speicherinhalt in memory_dict.items():
             memory_dict[speicherzelle] = speicherinhalt.strWort
+            #vermutlich muss hier eine Exception aufgefangen werden? -> dann memory_dict[speicherzelle]=None
 
         self.memory_list.add(memory_dict)
-  
-    def printMemory(self, old_step, current_step):
+    # mehrere Steps?
+    def printMemory(self,  current_step:int, mode="all", output="console", old_step: int=None ):
         '''
-        print state of memory and registers in relation to the status of the memory one step before
+        print state of memory and registers 
+        
+        Args:
+
+        current_step (int) = The current step 
+        mode (str) = ("all" or "changes") "all" when the whole memory (excluded the Speicherzellen containing 0) is needed, "changes" when the differences between the current step and any other step is needed (old_step required)
+        output (str) = ("console" or "csv") output method
+        old_step (int) = The step to which the changes should be compared
         
         '''
-       
-        memory_change = []
+        if mode=="all":
+            memory_output = []
+            for speicherzelle, speicherinhalt in self.memory_list[current_step].items():
+            
+                if speicherinhalt == 0:
+                    continue
+                else:
+                    memory_output.append([speicherzelle, speicherinhalt])   
+            if output=="csv":
+                with open("Step%s_memory_status.csv" %current_step, "w") as file:
+                    writer = csv.writer(file)
+                    writer.writerow(["Speicherzelle", current_step])
+                    writer.writerows(memory_output)
 
-      
-        for speicherzelle, old_speicherinhalt in self.memory_list[old_step].items():
-        
-            new_speicherinhalt = self.memory_list[current_step][speicherzelle]
-            if old_speicherinhalt == new_speicherinhalt:
-                continue
-            else:
-                memory_change.append([speicherzelle, old_speicherinhalt, new_speicherinhalt])
+            elif output=="console":
+                table = PrettyTable()
+                table.field_names = ["Speicherzelle",  current_step]
+                table.add_rows(memory_output)
 
+                print(table)     
         
-        with open("Step%s_memory_change.csv" %current_step, "w") as file:
-            writer = csv.writer(file)
-            writer.writerow(["Speicherzelle",old_step, current_step])
-            writer.writerows(memory_change)
-       
+        elif mode =="changes":
+            memory_change = []
+            if old_step == None:
+                raise ValueError("Missing old_step parameter")
+
+            for speicherzelle, old_speicherinhalt in self.memory_list[old_step].items():
+            
+                new_speicherinhalt = self.memory_list[current_step][speicherzelle]
+                if old_speicherinhalt == new_speicherinhalt:
+                    continue
+                else:
+                    memory_change.append([speicherzelle, old_speicherinhalt, new_speicherinhalt])
+            if output=="csv":
+                with open("Step%s_memory_change.csv" %current_step, "w") as file:
+                    writer = csv.writer(file)
+                    writer.writerow(["Speicherzelle",old_step, current_step])
+                    writer.writerows(memory_change)
+
+            elif output=="console":
+                table = PrettyTable()
+                table.field_names = ["Speicherzelle", old_step, current_step]
+                table.add_rows(memory_change)
+
+                print(table)
         
     
 def readProgram(txt):
@@ -114,15 +151,40 @@ if __name__ == '__main__':
             "7": "25",
         },
         {
-            "1": "25",
+            "1": None,
             "2": "25",
             "3": "25",
             "4": "25",
             "5": "25",
-            "6": "25",
+            "6": "26",
             "7": "25",
             
         }
         ]
-    io.printMemory(0,1)
+    io.printMemory(current_step=1, mode="changes", output="console", old_step=0)
 
+
+# Tests: pseudo Speicher übergeben
+# io.memory_list=[
+#         {
+#             "1": "23",
+#             "2": "24",
+#             "3": "25",
+#             "4": "25",
+#             "5": "25",
+#             "6": "25",
+#             "7": "25",
+#         },
+#         {
+#             "1": "25",
+#             "2": "25",
+#             "3": "25",
+#             "4": "25",
+#             "5": "25",
+#             "6": "25",
+#             "7": "25",
+            
+#         }
+#         ]
+
+# Test valider Programminput
--- printer.py
+++ printer.py
@@ -2,14 +2,26 @@ class ResultPrinter:
     def __init__(self):
         self.strToPrint =""
 
-    def collectPrint(self):
+    def collectPrint(self, accumulator):
         '''adds the accumulator to strToPrint'''
-        #self.strToPrint += memory.get()
+        self.strToPrint += accumulator.strWort + "\n"
 
         pass
-    def printAll(self):
-        '''prints all into a text file'''
-        #print(self.strToPrint)
-        with open("result.txt", "w") as f:
-            f.write(self.strToPrint)  
-        pass   
+    def printAll(self, mode="console"):
+        '''
+        prints content of strToPrint into a text file
+        
+        Args:
+        mode (str) = ("console" or "txt") output method
+
+        '''
+        if mode=="console":
+            print(self.strToPrint)
+        elif mode=="txt":
+            with open("result.txt", "w") as f:
+                f.write(self.strToPrint)  
+               
+
+
+# Test accumulator Klasse Wort
+#
